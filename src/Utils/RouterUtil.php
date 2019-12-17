ac6912d512ef27b2de4f90416d21d7f491c169e0
CyberSai <isaacsai030@gmail.com>
2019-12-17 00:27:20 +0000

    Added Function to RouterUtil

--- src/Utils/RouterUtil.php
+++ src/Utils/RouterUtil.php
@@ -4,11 +4,26 @@
 namespace Cybersai\USSD\Utils;
 
 
+use Cybersai\USSD\Constants\Hubtel;
 use Cybersai\USSD\Constants\Korba;
+use Cybersai\USSD\Constants\Nsano;
+use Cybersai\USSD\Constants\TxtGhana;
 
 final class RouterUtil
 {
     public static function getUserInputFromKorbaRequest($array) {
         return $array[Korba::user_input];
+    }
+
+    public static function getUserInputFromTxtGhanaRequest($array) {
+        return $array[TxtGhana::user_input];
+    }
+
+    public static function getUserInputFromNsanoRequest($array) {
+        return $array[Nsano::user_input];
+    }
+
+    public static function getUserInputFromHubtelRequest($array) {
+        return $array[Hubtel::user_input];
     }
 }

eb52f88963d07d71763e5de2cfc1e14b401a64bf
Benjamin Manford <benjaminmanford@gmail.com>
2020-01-26 10:02:55 +0000

    Fix typo

--- src/Exceptions/ViewNotFoundException.php
+++ src/Exceptions/ViewNotFoundException.php
@@ -7,7 +7,7 @@ class ViewNotFoundException extends \Exception
 {
     public function __construct()
     {
-        $message = "View it not an instance of either TemplateView, TemplateListView or TemplateViewValidator";
+        $message = "View is not an instance of either TemplateView, TemplateListView or TemplateViewValidator";
         $code = 0;
         $previous = null;
         parent::__construct($message, $code, $previous);
